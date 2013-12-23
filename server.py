d2b0f684c873f79707c78a7ed11c54a06b49d0ef
kiote <krivich.ekaterina@gmail.com>
2013-12-23 16:59:46 +0400

    show results on map

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+#    return 'hello'
+    return render_template('hello.html') 
 
 if __name__ == "__main__":
     app.run()
